30fa2d428a7b03e2100768fe99a050b99821673f
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-29 12:42:41 +0900

    数学Bの公式導出を共通表示へ接続

--- src/content/math1/formula-registry.ts
+++ src/content/math1/formula-registry.ts
@@ -1,5 +1,8 @@
+import { getMathBFormulaDerivation } from "../mathb/formula-derivations";
 import { getAdditionalFormulaDerivation } from "./additional-formula-derivations";
 import { getFormulaDerivation as getAlgebraFormulaDerivation } from "./formula-derivations";
 
 export const getFormulaDerivation = (lessonKey: string) =>
-  getAlgebraFormulaDerivation(lessonKey) ?? getAdditionalFormulaDerivation(lessonKey);
+  getAlgebraFormulaDerivation(lessonKey) ??
+  getAdditionalFormulaDerivation(lessonKey) ??
+  getMathBFormulaDerivation(lessonKey);
