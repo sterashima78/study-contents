286bd0eb7a003490fcbc08d5f468eb6eed0f8bce
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-30 10:38:27 +0900

    Expose practice input design in shared lesson types

--- src/content/math1/types.ts
+++ src/content/math1/types.ts
@@ -1,7 +1,15 @@
+import type { PracticeStep } from "../practice";
 import type { AlgebraUnit, Lesson } from "./algebra";
 
-export type MathUnit = AlgebraUnit;
-export type MathLesson = Lesson;
+export type MathLesson = Omit<Lesson, "practice"> & {
+  practice: Omit<Lesson["practice"], "steps"> & {
+    steps: PracticeStep[];
+  };
+};
+
+export type MathUnit = Omit<AlgebraUnit, "lessons"> & {
+  lessons: MathLesson[];
+};
 
 export type MathArea = {
   key: string;
