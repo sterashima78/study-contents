38260c20c59581383bfb002d17fa2c3b08c2a142
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-09-03 19:39:14 +0900

    Register signed number lesson diagrams

--- src/content/middle-school/math/grade1/lesson-diagrams.ts
+++ src/content/middle-school/math/grade1/lesson-diagrams.ts
@@ -1,10 +1,12 @@
 import { getMiddleMath1DataLessonDiagrams } from "./data-use-diagrams";
 import { getMiddleMath1LessonDiagrams as getMiddleMath1FunctionLessonDiagrams } from "./diagrams";
 import { getMiddleMath1GeometryLessonDiagrams } from "./geometry-diagrams";
+import { getMiddleMath1SignedNumberLessonDiagrams } from "./signed-number-diagrams";
 import { getMiddleMath1MeasurementLessonDiagrams } from "./solid-measurement-diagrams";
 import { getMiddleMath1SpatialLessonDiagrams } from "./spatial-geometry-diagrams";
 
 export const getMiddleMath1LessonDiagrams = (lessonKey: string) =>
+  getMiddleMath1SignedNumberLessonDiagrams(lessonKey) ??
   getMiddleMath1FunctionLessonDiagrams(lessonKey) ??
   getMiddleMath1GeometryLessonDiagrams(lessonKey) ??
   getMiddleMath1SpatialLessonDiagrams(lessonKey) ??
